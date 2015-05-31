6bfcbbd2829420960e19c6fc8db6d5e97c1cd182
Sade Christner <sadechristner@Sades-MBP.local.tld>
2015-05-31 02:54:29 -0700

    merging

--- config/config.js
+++ config/config.js
@@ -1,4 +1,3 @@
-
 // This is a sample config.js file where you will put the tokens that you get from Uber
 // remember to change these AND change the name of the file to config.js after you are done
 module.exports = {
@@ -6,3 +5,4 @@ module.exports = {
   ClientSecret: "BB_W-vSg8JLtSvasPxrBM6PxNrSehuNd21MQaA3m",
   ServerID: "KObSgoSwQiHP3rObYaSXy2TCoWgW1ghpSwu8whvZ",
 }
+
